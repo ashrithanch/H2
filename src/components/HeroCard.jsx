7e9d04b4f1e781883f6c6d4e9d2ba33a92a170aa
Ashrithanch09 <151167244+Ashrithanch09@users.noreply.github.com>
2024-04-23 16:29:43 +0530

    Update HeroCard.jsx

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -2,7 +2,7 @@ const HeroCard = ({ img, heading }) => {
   return (
     <div className="flex flex-col w-[220px] [box-shadow:rgba(99,_99,_99,_0.2)_0px_2px_8px_0px] items-center h-[300px] cursor-pointer hover:scale-105">
       <div className="w-full h-[140px] ">
-        <img src={img} alt="" className="w-full h-full" />
+        <img src={pic1.jpg} alt="" className="w-full h-full" />
       </div>
 
       <div className=" p-6 flex flex-col gap-[15px]">
