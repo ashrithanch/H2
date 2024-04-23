ed79523a8ce9dd44064c9b7ab4d6dbd57a84c936
Ashrithanch09 <151167244+Ashrithanch09@users.noreply.github.com>
2024-04-23 16:53:42 +0530

    Update HeroCard.jsx

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -10,6 +10,9 @@ const HeroCard = ({ img, heading }) => {
           The market capitalisation 
           of Indian stocks crossed US $4.5 trillion in January
         </p>
+        <img src = "pic3.jpg">
+          <p>The last few years have seen a very substantial step up in capital</p>
+        </img>
       </div>
     </div>
   )
