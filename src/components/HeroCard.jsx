143f94fb0078d74af60545dbe216de46165157a6
Ashrithanch09 <151167244+Ashrithanch09@users.noreply.github.com>
2024-04-23 14:58:25 +0530

    Update HeroCard.jsx

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -12,17 +12,6 @@ const HeroCard = ({ img, heading }) => {
           of Indian stocks crossed US $4.5 trillion in January
         </p>
       </div>
-      <div className="flex flex-col w-[220px] [box-shadow:rgba(99,_99,_99,_0.2)_0px_2px_8px_0px] items-center h-[300px] cursor-pointer hover:scale-105">
-      <div className="w-full h-[140px] ">
-        <img src={img} alt="" className="w-full h-full" />
-      </div>
-
-      <div className=" p-6 flex flex-col gap-[15px]">
-        <h3 className="font-[500] text-[14px]">{heading}</h3>
-        <p className="text-black/40 text-[12px]">
-          The last few years have seen a very substantial step up in capital
-        </p>
-      </div>
   )
 }
 export default HeroCard
